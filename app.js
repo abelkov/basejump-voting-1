37003f80d37a42c2be015735ed9e283709cffffe
Alexey Belkov <knots@ya.ru>
2015-05-16 23:50:44 +0300

    Use jade and stylus; add a home route and error handlers

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
 var express = require('express')
-  , morgan = require('morgan');
+  , morgan = require('morgan')
+  , stylus = require('stylus')
+  , nib = require('nib')
+  , path = require('path');
 
 var app = express();
 app.use(morgan('dev'));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(stylus.middleware({
+    src: __dirname
+  , compile: compile
+}));
+
+function compile(str, path) {
+  return stylus(str)
+    .define('url', stylus.url({
+      paths : [__dirname + "/public"],
+      limit : 10000
+    }))
+    .set('filename', path)
+    .set('compress', true)
+    .use(nib());
+}
+
+app.use(express.static(__dirname + '/public'));
+
+
+/**** Routes ****/
+
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  res.render('home');
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next) {
+  res.status(404);
+  res.send('404 Error');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.send('500 Error');
 });
 
 app.listen(3000, function() {
