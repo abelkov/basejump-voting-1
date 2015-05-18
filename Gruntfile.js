5c81c0cc3bfb51142b5f1cd80b7ebd337d8a9f31
Alexey Belkov <knots@ya.ru>
2015-05-18 19:29:33 +0300

    Rewrite grunt watch paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
     watch: {
       livereload: {
         options: { livereload: true },
-        files: '**/*'
+        files: ['./*.js', 'views/**/*.jade', 'public/stylesheets/*.styl']
       }
     }
 
