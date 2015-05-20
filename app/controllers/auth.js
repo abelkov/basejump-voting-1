946611da3ffe45a11c0001e400ce3c647389b502
Alexey Belkov <knots@ya.ru>
2015-05-20 19:23:17 +0300

    Handle user signup

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,21 +1,33 @@
+var User = require('mongoose').model('User');
+
 exports.renderAuthorize = function(req, res) {
   res.render('authorize', {
     path: req.path
   });
 };
 
-exports.signup = function(req, res) {
-  // var name = req.body.name
-  //   , email = req.body.email
-  //   , password = req.body.password;
+exports.signup = function(req, res, next) {
+  var user = new User({
+    name: req.body.name
+  , email: req.body.email
+  , password: req.body.password
+  });
 
-  req.session.flash = {
-    type: 'info'
-  , intro: 'Thanks for trying...'
-  , message: 'Sign up coming real soon now!'
-  }
+  user.save(function(err, user) {
+    if (err) {
+      return next(err);
+    } else {
+      req.session.flash = {
+        type: 'success'
+      , intro: 'Great!'
+      , message: 'You\'re all signed up now.'
+      };
 
-  res.redirect(303, '/');
+      console.log(user);
+
+      res.redirect(303, '/');
+    }
+  });
 };
 
 exports.login = function(req, res) {
